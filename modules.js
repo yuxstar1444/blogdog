b1cd38ec0ac2e2c3efe7894dac9a02275b2d719c
yuxstar1444@protonmail.com <yuxstar1444@protonmail.com>
2024-03-11 16:52:54 -0400

    first commit

---
+++ modules.js
@@ -0,0 +1,4 @@
+// const xyz = require('./people');
+const { people, ages} = require('./people');
+
+console.log(xyz) //returns an empty object
---
+++ people.js
@@ -0,0 +1,11 @@
+const people = ['yoshi', 'ryu', 'chun', 'mario']
+const ages = [20, 25, 30, 35]
+
+console.log(people);
+
+module.exports = {
+    //people:people
+    people, ages
+
+
+};
